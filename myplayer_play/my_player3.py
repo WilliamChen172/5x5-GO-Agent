bd24caf58edcd229e4b4fc11a3eaff2399c09cfc
William Chen <chenyuwilliam@gmail.com>
2021-10-31 22:05:17 -0700

    Optimized iterative deepening
    Changed evaluation method for both black and white

--- myplayer_play/my_player3.py
+++ myplayer_play/my_player3.py
@@ -1,7 +1,6 @@
-import random
-import sys
-from copy import deepcopy
 import cProfile
+import time
+import os
 
 
 class MyGO():
@@ -19,7 +18,6 @@ class MyGO():
                 if prev_board[i][j] == type and board[i][j] != type:
                     self.dead.append((i, j))
 
-        # self.piece_type = piece_type
         self.prev_board = prev_board
         self.board = board
 
@@ -60,18 +58,26 @@ class MyGO():
     def find_liberty(self, i, j, board):
         # Adapted from host.py
         ally_members = self.ally_dfs(i, j, board)
-        # if board[0][0] == 2 and board[1][0] == 1 and board[0][1] == 1:
-        # print(i, j)
-        # print(ally_members)
-        # go.visualize_board(board)
         for member in ally_members:
             neighbors = self.detect_neighbor(member[0], member[1], board)
             for piece in neighbors:
                 # If there is empty space around a piece, it has liberty
                 if board[piece[0]][piece[1]] == 0:
+
                     return True
         # If none of the pieces in a allied group has an empty space, it has no liberty
         return False
+
+    def find_liberties(self, i, j, board):
+        # Adapted from host.py
+        liberties = 0
+        neighbors = self.detect_neighbor(i, j, board)
+        for piece in neighbors:
+            # If there is empty space around a piece, it has liberty
+            if board[piece[0]][piece[1]] == 0:
+                liberties += 1
+        # If none of the pieces in a allied group has an empty space, it has no liberty
+        return liberties
 
     def find_died_pieces(self, piece_type, board):
         # Adapted from host.py
@@ -91,19 +97,14 @@ class MyGO():
         died_pieces = self.find_died_pieces(piece_type, board)
         if not died_pieces:
             return []
-        # print("died: ", died_pieces)
         for piece in died_pieces:
             board[piece[0]][piece[1]] = 0
-        # go.visualize_board(board)
         return died_pieces
 
     def place(self, i, j, piece_type, board):
-        board = deepcopy(board)
+        board = list(map(list, board))
         board[i][j] = piece_type
         self.remove_died_pieces(3 - piece_type, board)
-        # if board[0][1] == 1 and board[1][0] == 1:
-        #     print("place")
-        #     go.visualize_board(board)
         return board
 
     def compare_board(self, board1, board2):
@@ -117,8 +118,7 @@ class MyGO():
     def valid_place_check(self, i, j, piece_type, board):
         # Adapted from host.py
         verbose = False
-        # if test_check:
-        #     verbose = False
+
         # Check if the place is in the board range
         if not (i >= 0 and i < len(board)):
             if verbose:
@@ -136,25 +136,25 @@ class MyGO():
             return False
 
         # Copy the board for testing
-        test_go = deepcopy(self)
-        test_board = deepcopy(board)
+        test_board = list(map(list, board))
 
         # Check if the place has liberty
         test_board[i][j] = piece_type
-        test_go.board = test_board
-        if test_go.find_liberty(i, j, test_board):
+
+        if self.find_liberty(i, j, test_board):
             return True
 
         # If not, remove the died pieces of opponent and check again
-        test_go.remove_died_pieces(3 - piece_type, board)
-        if not test_go.find_liberty(i, j, test_board):
+        self.remove_died_pieces(3 - piece_type, test_board)
+
+        if not self.find_liberty(i, j, test_board):
             if verbose:
                 print('Invalid placement. No liberty found in this position.')
             return False
 
         # Check special case: repeat placement causing the repeat board state (KO rule)
         else:
-            if self.died_pieces and self.compare_board(self.previous_board, test_go.board):
+            if self.dead and self.compare_board(self.prev_board, test_board):
                 if verbose:
                     print('Invalid placement. A repeat move not permitted by the KO rule.')
                 return False
@@ -177,36 +177,52 @@ class MyGO():
             right = True
         return isEmpty and up and down and left and right
 
-    def has_neighbour(self, i, j, board):
+    def has_neighbour(self, i, j, type, board):
         empty = 0
         for x in range(self.size):
             for y in range(self.size):
                 if board[x][y] == 0:
                     empty += 1
         # print(empty)
-        if empty < 14:
+        if empty < 14 or empty == 25:
             return True
         neighbours = self.detect_neighbor(i, j, board)
         if i > 0 and j > 0:
-            neighbours.append((i-1, j-1))
-        if i > 0 and j < len(board)-1:
-            neighbours.append((i-1, j+1))
-        if i < len(board)-1 and j > 0:
-            neighbours.append((i+1, j-1))
-        if i < len(board)-1 and j < len(board)-1:
-            neighbours.append((i+1, j+1))
+            neighbours.append((i - 1, j - 1))
+        if i > 0 and j < len(board) - 1:
+            neighbours.append((i - 1, j + 1))
+        if i < len(board) - 1 and j > 0:
+            neighbours.append((i + 1, j - 1))
+        if i < len(board) - 1 and j < len(board) - 1:
+            neighbours.append((i + 1, j + 1))
         # print(i, j, neighbours)
         for n in neighbours:
-            if board[n[0]][n[1]] > 0:
+            if board[n[0]][n[1]] == 3 - type:
                 return True
         return False
 
     def reward(self, piece_type, board):
         score = 0
-        for i in range(self.size):
-            for j in range(self.size):
-                if board[i][j] == piece_type or self.is_territory(piece_type, i, j, board):
-                    score += 1
+        if piece_type == 1:
+            for i in range(self.size):
+                for j in range(self.size):
+                    if board[i][j] == piece_type:
+                        score += 1
+                    # if self.is_territory(piece_type, i, j, board):
+                    #     score += 0.5
+                    if board[i][j] == 3 - piece_type:
+                        score -= 1
+                    # if self.is_territory(3 - piece_type, i, j, board):
+                    #     score -= 0.5
+        if piece_type == 2:
+            for i in range(self.size):
+                for j in range(self.size):
+                    if board[i][j] == piece_type:
+                        score += 1
+                        score += 0.1 * self.find_liberties(i, j, board)
+                    if self.is_territory(piece_type, i, j, board):
+                        score += 0.5
+
         # Implement Benson's unconditional live
 
         # print("Score:", score, "\n")
@@ -234,91 +250,122 @@ class Node:
         self.step = step
         self.next_step = next_step
         self.reward = -1000
+        self.best_path = []
 
+
+# class TranspositionTable:
 
 class Minimax:
-    def __init__(self, go, root):
+    def __init__(self, go, root, depth):
         self.go = go
         self.root = root
+        self.best_move = []
+        self.depth = depth
 
-    def max_value(self, depth, cur_node):
+    def max_value(self, depth, cur_node, alpha, beta):
         type = cur_node.type
         board = cur_node.board
         go = self.go
         cur_node.reward = -1000
-        alpha = -1000
-        beta = 1000
         if depth == 0:
             cur_node.reward = go.reward(self.root.type, cur_node.board)
+            cur_node.best_path = [cur_node.step]
             return cur_node
 
         # find possible placements
-        possible_placements = []
+        psb_placements = []
         for i in range(go.size):
             for j in range(go.size):
-                if go.valid_place_check(i, j, type, board) and go.has_neighbour(i, j, board):
-                    possible_placements.append((i, j))
-        # if depth == 4:
-        #     print(possible_placements)
-        if not possible_placements:
+                if go.valid_place_check(i, j, type, board) and go.has_neighbour(i, j, type, board):
+                    psb_placements.append((i, j))
+
+        # print(go.valid_place_check(3, 1, 1, board))
+        # print(psb_placements)
+        if not psb_placements:
             cur_node.reward = go.reward(self.root.type, cur_node.board)
+            cur_node.best_path = [cur_node.step]
             return cur_node
         depth -= 1
         new_type = 3 - type
-        for (i, j) in possible_placements:
-            # print("Placing", stone, "at", i, j, "...")
+        if len(self.best_move) > 0 and self.best_move[0] in psb_placements:
+            psb_placements.insert(0, psb_placements.pop(psb_placements.index(self.best_move.pop(0))))
+            # print(psb_placements, cur_node.step)
+        for (i, j) in psb_placements:
             new_board = go.place(i, j, type, board)
-            # go.visualize_board(new_board)
-            # print("Calculating reward...")
             new_child = Node(new_board, new_type, (i, j))
-            new_child = self.min_value(depth, new_child)
+            new_child = self.min_value(depth, new_child, alpha, beta)
             if new_child.reward > cur_node.reward:
                 cur_node.reward = new_child.reward
                 cur_node.next_step = new_child.step
+                # print("max", depth)
+                if depth + 1 == self.depth:
+                    cur_node.best_path = new_child.best_path
+                else:
+                    cur_node.best_path = [cur_node.step] + new_child.best_path
+                # print(cur_node.best_path)
             if new_child.reward >= beta:
                 return cur_node
             if new_child.reward > alpha:
                 alpha = new_child.reward
         return cur_node
 
-    def min_value(self, depth, cur_node):
+    def min_value(self, depth, cur_node, alpha, beta):
         type = cur_node.type
         board = cur_node.board
         go = self.go
         cur_node.reward = 1000
-        alpha = -1000
-        beta = 1000
         if depth == 0:
             cur_node.reward = go.reward(self.root.type, cur_node.board)
+            cur_node.best_path = [cur_node.step]
             return cur_node
 
         # find possible placements
-        possible_placements = []
+        psb_placements = []
         for i in range(go.size):
             for j in range(go.size):
-                if go.valid_place_check(i, j, type, board) and go.has_neighbour(i, j, board):
-                    possible_placements.append((i, j))
-        if not possible_placements:
+                if go.valid_place_check(i, j, type, board) and go.has_neighbour(i, j, type, board):
+                    psb_placements.append((i, j))
+        if not psb_placements:
             cur_node.reward = go.reward(self.root.type, cur_node.board)
+            cur_node.best_path = [cur_node.step]
             return cur_node
 
         depth -= 1
         new_type = 3 - type
-        for (i, j) in possible_placements:
-            # print("Placing", stone, "at", i, j, "...")
+        if len(self.best_move) > 0 and self.best_move[0] in psb_placements:
+            psb_placements.insert(0, psb_placements.pop(psb_placements.index(self.best_move.pop(0))))
+            # print(psb_placements, cur_node.step)
+        for (i, j) in psb_placements:
             new_board = go.place(i, j, type, board)
-            # go.visualize_board(new_board)
-            # print("Calculating reward...")
             new_child = Node(new_board, new_type, (i, j))
-            new_child = self.max_value(depth, new_child)
+            new_child = self.max_value(depth, new_child, alpha, beta)
             if new_child.reward < cur_node.reward:
                 cur_node.reward = new_child.reward
                 cur_node.next_step = new_child.step
+                # print("min", depth)
+                if depth + 1 == self.depth:
+                    cur_node.best_path = new_child.best_path
+                else:
+                    cur_node.best_path = [cur_node.step] + new_child.best_path
+                # print(cur_node.best_path)
             if new_child.reward <= alpha:
                 return cur_node
             if new_child.reward < beta:
                 beta = new_child.reward
         return cur_node
+
+    def iterative_deepening(self, root):
+        start_time = time.time()
+        depth = self.depth
+        for i in range(1, depth + 1):
+            self.depth = i
+            root = self.max_value(i, root, -1000, 1000)
+            self.best_move = root.best_path
+            # print("id", self.best_move)
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= 2000:
+                break
+        return root
 
 
 class MyPlayer:
@@ -326,26 +373,28 @@ class MyPlayer:
         self.type = 'random'
         self.depth = depth
 
-    def get_input(self, go, piece_type, board):
+    def get_input(self, go, piece_type, board, moves):
         possible_placements = []
         for i in range(go.size):
             for j in range(go.size):
-                if go.valid_place_check(i, j, piece_type, board):
+                if board[i][j] == 0:
                     possible_placements.append((i, j))
-        if not possible_placements:
-            return "Pass"
-        # print(possible_placements)
         if len(possible_placements) == 25:
             return (2, 2)
         root = Node(go.board, piece_type, None)
         # go.visualize_board(root.board)
-        minimax = Minimax(go, root)
-        root = minimax.max_value(self.depth, root)
+        if moves < self.depth:
+            self.depth = moves
+        minimax = Minimax(go, root, self.depth)
+        root = minimax.iterative_deepening(root)
         print(root.next_step)
         go.visualize_board(root.board)
+        if not root.next_step:
+            return "PASS"
         return root.next_step
 
 
+# Adapted from read.py
 def readInput(n, path="input.txt"):
     with open(path, 'r') as f:
         lines = f.readlines()
@@ -358,6 +407,7 @@ def readInput(n, path="input.txt"):
         return piece_type, previous_board, board
 
 
+# Adapted from write.py
 def writeOutput(result, path="output.txt"):
     res = ""
     if result == "PASS":
@@ -369,11 +419,31 @@ def writeOutput(result, path="output.txt"):
         f.write(res)
 
 
+def readMoves(piece_type, path="moves.txt"):
+    if not os.path.exists(path):
+        moves = 25-piece_type
+        with open(path, 'w') as f:
+            f.write(str(moves - 2))
+            return moves
+    with open(path, 'r') as g:
+        moves = int(g.read())
+        g.close()
+    with open(path, 'w') as h:
+        h.write(str(moves - 2))
+        if moves - 2 <= 0:
+            h.close()
+            os.remove(path)
+        return moves
+
+
 if __name__ == "__main__":
     N = 5
     piece_type, previous_board, board = readInput(N)
+    moves = readMoves(piece_type)
+    print(moves)
     go = MyGO(N)
     go.set_board(piece_type, previous_board, board)
-    player = MyPlayer(depth=4)
-    action = player.get_input(go, piece_type, board)
+    player = MyPlayer(depth=6)
+    # cProfile.run("player.get_input(go, piece_type, board)")
+    action = player.get_input(go, piece_type, board, moves)
     writeOutput(action)
